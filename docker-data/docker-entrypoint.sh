5cb751a2c57f2848a209b71ff8bb9b6deec4104d
mikecaat <righttoleft1134@gmail.com>
2020-10-10 08:15:33 +0900

    update replicas from 0 to 1

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'redis-cluster' ]; then
     sleep 10
-    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 0 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002
+    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002 173.17.0.5:7003 173.17.0.6:7004 173.17.0.7:7005
     echo "DONE"
 else
   exec "$@"
